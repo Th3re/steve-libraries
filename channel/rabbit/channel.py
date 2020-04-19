19b5070b2fc663156f33a3c012b28dcc197f2bdd
Mateusz Stompór <mateusz.stompor@ibm.com>
2020-04-19 18:59:14 +0200

    Simplify conditional assignment of the topic

--- channel/rabbit/channel.py
+++ channel/rabbit/channel.py
@@ -32,9 +32,7 @@ class RabbitChannel(Channel):
 
     def _send_attempt(self, topic, data) -> ChannelResponse:
         self.channel.exchange_declare(exchange=self.exchange, exchange_type=self.__exchange_type)
-        routing_key = f'{self.topic}.{topic}'
-        if self.topic == '':
-            routing_key = topic
+        routing_key = topic if self.topic == '' else f'{self.topic}.{topic}'
         self.channel.basic_publish(
             exchange=self.exchange,
             routing_key=routing_key,
