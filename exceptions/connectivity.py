e98a9634095fc5d4a75f98281ed5887d320c18db
Piotr Persona <persona.piotr@gmail.com>
2020-05-27 21:03:53 +0200

    Add str to API Exception
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- exceptions/connectivity.py
+++ exceptions/connectivity.py
@@ -5,8 +5,14 @@ class APIError(Exception):
     def __init__(self, response: requests.Response):
         self.response = response
 
-    def __repr__(self):
+    def __repr(self):
         return f'<{self.__class__.__name__}>[status_code: \"{self.response.status_code}\", content: \"{self.response.content}\"]'
+
+    def __repr__(self):
+        return self.__repr()
+
+    def __str__(self):
+        return self.__repr()
 
 
 class AuthenticationError(APIError):
